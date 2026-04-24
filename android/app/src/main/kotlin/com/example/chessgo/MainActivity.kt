0912e18509309b0a0c15ca5eb6be5f1987c84d10
Marco Antonio Diaz Diaz <kingmarkdc@gmail.com>
2026-04-24 16:01:22 -0600

    Add

---
+++ android/app/src/main/kotlin/com/example/chessgo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chessgo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChessGo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('ChessGo')),
+        body: Center(child: Container(width: 100, height: 100, color: Colors.red)),
+      ),
+    );
+  }
+}
